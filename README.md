d4f2ff332cb7850e70ec3dc75be317d4d1b0ae46
Daylong <daylong@38f9d3567566.ant.amazon.com>
2019-11-07 16:36:54 -0800

    completed lab 03

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # About-Dayne
+
+Change log:
+
+11/07/2019
+- Created "Dayne's Top Video Games" ordered list
+- Converted "About Me" Paragraph into an Unordered list
+- Created "while loop" for question 6
+- Created "for loop" for question 7
+- Created "correct answer counter" w/display at the end
--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-'use strict'; // no global var
+'use strict';
 
+var correctanswer = 0
 
 // capture name
 var user = prompt('Hello There! What is your name?');
 console.log('the users name is ' + user);
 
 // send the user a greeting w/ guessing game rules
-alert('Hello ' + user + '. To continue onto this site, please complete this guessing game about Dayne. Please answer "yes" or "no".');
+alert('Hello ' + user + '. To continue onto this site, please complete this guessing game about Dayne.');
 
 // capture more user input, then send back if they got it right or wrong
 var origin = prompt('Is Dayne from Seattle?');
 if (origin === 'y' || origin === 'yes' || origin === 'n' || origin === 'no'){
     origin = origin.toUpperCase()
+    console.log(origin + ', Dayne is from Seattle.');
 }
 if (origin === 'YES'){
     alert('Correct.')
+    correctanswer++;
 }else{
     alert('Incorrect.')
 }
@@ -23,9 +26,11 @@ if (origin === 'YES'){
 var movie = prompt('Does Dayne like Star Wars?');
 if (movie === 'y' || movie === 'yes' || movie === 'n' || movie === 'no'){
     movie = movie.toUpperCase()
+    console.log(movie + ', Dayne likes Star Wars.');
 }
 if (movie === 'YES'){
     alert('Correct.')
+    correctanswer++;
 }else{
     alert('Incorrect.')
 }
@@ -34,9 +39,11 @@ if (movie === 'YES'){
 var car = prompt('Does Dayne drive a BMW?')
 if (car === 'y' || car === 'yes' || car === 'n' || car === 'no'){
     car = car.toUpperCase()
+    console.log(car + ', Dayne drives the Ultimate Driving Machine.');
 }
 if (car === 'YES'){
     alert('Correct.')
+    correctanswer++;
 }else{
     alert('Incorrect.')
 }
@@ -45,9 +52,11 @@ if (car === 'YES'){
 var hoowah = prompt('Was Dayne an Army Ranger?')
 if (hoowah === 'y' || hoowah === 'yes' || hoowah === 'n' || hoowah === 'no'){
     hoowah = hoowah.toUpperCase()
+    console.log(hoowah + ', Dayne was not in the Army.');
 }
 if (hoowah === 'NO'){
     alert('Correct.')
+    correctanswer++;
 }else{
     alert('Incorrect.')
 }
@@ -56,11 +65,55 @@ if (hoowah === 'NO'){
 var school = prompt('Has Dayne been to college?')
 if (school === 'y' || school === 'yes' || school === 'n' || school === 'no'){
     school = school.toUpperCase()
+    console.log(school + ', Dayne did not go to school.');
 }
 if (school === 'NO'){
     alert('Correct.')
+    correctanswer++;
 }else{
-    alert('Incorrect.')
+    alert('Incorrect.');
 }
 
-alert('Thank you ' + user + '. Enjoy reading more about me.')
+// question 6
+
+var favnum = 5;
+var counter = 4;
+
+while (counter > 0) {
+
+var numGuess = prompt('What is my favorite number?');    
+var numGuessInt = parseInt(numGuess);
+
+    if (numGuessInt < favnum) {
+        alert('Too low.');
+        counter--;
+    } else if (numGuessInt > favnum) {
+        alert('Too High.')
+        counter--;
+    } else if (numGuessInt === NaN || numGuessInt === null) {
+        alert('Please enter a real number.')
+    } else if (numGuessInt === favnum){
+        correctanswer++;
+        break;
+    }
+}
+
+// question 7
+
+var carBrand = ['bmw', 'lada', 'toyota','nissan'];
+
+for (var i = 5; i >= 0; i--){
+
+    var carBrandInt = prompt('What is my favorite brand of car?');
+    if (carBrand.includes(carBrandInt)){
+        alert('That is correct!');
+        correctanswer++;
+        break;
+    } else {
+        alert('That is wrong!');
+    }
+}
+
+
+
+alert('Thank you ' + user + '. You guessed ' + correctanswer + ' out of 7 correctly. I hope you enjoy my creation!');
